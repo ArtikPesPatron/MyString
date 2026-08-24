3fbf833c4cc44e2d4e589f2026c5603fe66cf2f2
pwspatronn <temich2288423@gmail.com>
2026-08-24 16:10:39 +0500

    Added handling for the case where attempts to process data_ == nullptr

--- MyString.cpp
+++ MyString.cpp
@@ -47,24 +47,28 @@ MyString::~MyString()
     delete[] data_;
 }
 
+const char* MyString::c_str() const {
+    return data_ != nullptr ? data_ : "";
+}
+
 void MyString::append(const MyString& other)
 {
+    const char* self = c_str();
+    const char* rhs = other.c_str();
+
     const auto new_data = new char[length_ + other.length_ + 1];
-    std::strcpy(new_data, data_);
-    std::strcat(new_data, other.data_);
-    delete[] data_;
+    std::strcpy(new_data, self);
+    std::strcat(new_data, rhs);
+
+    delete[] new_data;
     data_ = new_data;
     length_ += other.length_;
 }
 
 void MyString::print() const {
-    std::cout << data_ << '\n';
+    std::cout << c_str() << '\n';
 }
 
 size_t MyString::length() const {
     return length_;
-}
-
-const char *MyString::c_str() const {
-    return data_;
 }
--- MyString.hpp
+++ MyString.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstddef>
+
 class MyString {
 public:
     MyString();
